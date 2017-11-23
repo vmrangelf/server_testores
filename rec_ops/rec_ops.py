c5a7bc79625371c9c80a0f095008d0b231c4d2f6
vmrangelf <vmrangelf@gmail.com>
2017-11-23 16:39:13 -0600

    Aplicacion recursiva de operadores leidos de json

---
+++ rec_ops/rec_ops.py
@@ -0,0 +1,40 @@
+import json
+from catalogo import *
+from operadores import *
+
+
+def recorrido(arbol,pila):
+	if "expr" in arbol.keys():
+		pila=conc(arbol["expr"],pila);op=arbol["val"]
+		if op == 'P':
+			obj=operadores(recorrido(pila[0],[]),[[],[]])
+			aplica_operador(obj,op,int(arbol["expt"]))
+		else:
+			obj=operadores(recorrido(pila[0],pila[1:]),recorrido(pila[1],[]))
+			aplica_operador(obj,op)
+		return [obj.matriz,obj.ttm]
+	else:
+		return [dic[arbol["val"]]["mat"], dic[arbol["val"]]["tt"]]
+
+def aplica_operador(obj,op,n=None):
+	if op == 'T':
+		obj.theta()
+	elif op == 'P':
+		obj.phi(n)
+	elif op == 'G':
+		obj.gamma()
+	else:
+		print("error")
+
+def conc(lista1,lista2):
+	l1=lista1[:];l2=lista2[:]
+	l1.reverse()
+	for el in l1:
+		l2.insert(0,el)
+	return l2
+
+if __name__ == '__main__':
+	dic=read_csv_file("datos.csv")
+	data = json.load(open('data2.json'))
+	r=recorrido(data,[])
+	print(r)
