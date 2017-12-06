7072968858741f2e65a3df9c0e2adaddcc1d351b
vmrangelf <vmrangelf@gmail.com>
2017-12-06 16:54:55 -0600

    modificacion rec_ops

--- rec_ops/rec_ops.py
+++ rec_ops/rec_ops.py
@@ -1,40 +1,54 @@
-import json
-from catalogo import *
-from operadores import *
+from catalogo import read_csv_file
+from operadores import operadores
 
 
-def recorrido(arbol,pila):
-	if "expr" in arbol.keys():
-		pila=conc(arbol["expr"],pila);op=arbol["val"]
-		if op == 'P':
-			obj=operadores(recorrido(pila[0],[]),[[],[]])
-			aplica_operador(obj,op,pila[1]["val"])
-		else:
-			obj=operadores(recorrido(pila[0],pila[1:]),recorrido(pila[1],[]))
-			aplica_operador(obj,op)
-		return [obj.matriz,obj.ttm]
-	else:
-		return [dic[arbol["val"]]["mat"], dic[arbol["val"]]["tt"]]
+def recorrido(arbol, pila, dic):
+        if "expr" in arbol.keys():
+                pila = conc(arbol["expr"], pila)
+                op = arbol["val"]
+                if op == 'P':
+                        obj = operadores(recorrido(pila[0], [], dic), [[], []])
+                        aplica_operador(obj, op, pila[1]["val"])
+                else:
+                        obj = operadores(recorrido(pila[0], pila[1:], dic),
+                                         recorrido(pila[1], [], dic))
+                        aplica_operador(obj, op)
+                return [obj.matriz, obj.ttm]
+        else:
+                return [dic[arbol["val"]]["mat"], dic[arbol["val"]]["tt"]]
 
-def aplica_operador(obj,op,n=None):
-	if op == 'T':
-		obj.theta()
-	elif op == 'P':
-		obj.phi(n)
-	elif op == 'G':
-		obj.gamma()
-	else:
-		print("error")
 
-def conc(lista1,lista2):
-	l1=lista1[:];l2=lista2[:]
-	l1.reverse()
-	for el in l1:
-		l2.insert(0,el)
-	return l2
+def aplica_operador(obj, op, n=None):
+        if op == 'T':
+                obj.theta()
+        elif op == 'P':
+                obj.phi(n)
+        elif op == 'G':
+                obj.gamma()
+        else:
+                print("error")
 
-if __name__ == '__main__':
-	dic=read_csv_file("datos.csv")
-	data = json.load(open('data4.json'))
-	r=recorrido(data,[])
-	print(r)
+def conc(lista1, lista2):
+        l1 = lista1[:]
+        l2 = lista2[:]
+        l1.reverse()
+        for el in l1:
+                l2.insert(0, el)
+        return l2
+
+def to_dic(res):
+        dic = {"nombre": "resulatado", "matriz": res[0], "tt": res[1],
+               "ref": "Matriz resultente"}
+        return dic
+
+
+def solve(var):
+        dic = read_csv_file("datos.csv")
+        r = recorrido(var, [], dic)
+        return to_dic(r)
+
+if __name_  == '__main__':
+        d = {"val": "G", "tipo": "Op",
+             "expr": [{"val": "minima", "tipo": "Mat"},
+                      {"val": "h1", "tipo": "Mat"}]}
+        print(solve(d))
